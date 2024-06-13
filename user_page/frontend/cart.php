6bd79de16a6c30c27804d96b9c3b35178676f8f3
Ngocmai <ngocmai@gmail.com>
2024-06-14 00:36:45 +0700

    origin

---
+++ user_page/frontend/cart.php
@@ -0,0 +1,98 @@
+<?php include("header.php"); ?>
+
+
+      <!-- blog section start -->
+      
+    <section class="h-100 h-custom" style="background-color: white;">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12">
+            <div class="card card-registration card-registration-2" style="border-radius: 15px;">
+              <div class="card-body p-0">
+                <div class="row g-0">
+                 
+                  <div class="col-lg-4 bg-grey">
+                    <div class="p-5">
+                      <h3 class="fw-bold mb-5 mt-2 pt-1">Summary</h3>
+                      <hr class="my-4">
+    
+                      <div class="d-flex justify-content-between mb-4">
+                        <h5 class="text-uppercase">items 3</h5>
+                        <h5>€ 132.00</h5>
+                      </div>
+    
+                      <h5 class="text-uppercase mb-3">Shipping</h5>
+    
+                      <div class="mb-4 pb-2">
+                        <select data-mdb-select-init>
+                          <option value="1">Standard-Delivery- €5.00</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                          <option value="4">Four</option>
+                        </select>
+                      </div>
+    
+                      <h5 class="text-uppercase mb-3">Name</h5>
+    
+                      <div class="mb-5">
+                        <div data-mdb-input-init class="form-outline">
+                          <input type="text" id="form3Examplea2" class="form-control form-control-lg" />
+                          <!-- <label class="form-label" for="form3Examplea2">Enter your code</label> -->
+                        </div>
+                      </div>
+
+
+                      <h5 class="text-uppercase mb-3">Address</h5>
+    
+                      <div class="mb-5">
+                        <div data-mdb-input-init class="form-outline">
+                          <input type="text" id="form3Examplea2" class="form-control form-control-lg" />
+                          <!-- <label class="form-label" for="form3Examplea2">Enter your address</label> -->
+                        </div>
+                      </div>
+
+                      <h5 class="text-uppercase mb-3">Phone</h5>
+    
+                      <div class="mb-5">
+                        <div data-mdb-input-init class="form-outline">
+                          <input type="text" id="form3Examplea2" class="form-control form-control-lg" />
+                          <!-- <label class="form-label" for="form3Examplea2">Enter your address</label> -->
+                        </div>
+                      </div>
+
+                       <!-- "contact_taital" -->
+                      <hr class="my-4">
+    
+                      <div class="d-flex justify-content-between mb-5">
+                        <h5 class="text-uppercase">Total price</h5>
+                        <h5>€ 137.00</h5>
+                      </div>
+                      <div class="row mb-5">
+                        <div class="col-md-6">
+
+                          <h4 class="text-uppercase mb-3">Payment Method</h4>
+                          <h4 class="text-uppercase mb-3">Payment Method</h>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name=" paymentMethod" id="paymentCash" value="cash" checked>
+                            <label class="form-check-label" for="paymentCash">Cash</label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name=" paymentMethod" id="paymentQR" value="qr">
+                            <label class="form-check-label" for="paymentQR">QR Code</label>
+                          </div>
+                        </div>
+                      </div>
+                      
+    
+                      <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-block btn-lg"
+                        data-mdb-ripple-color="dark">Buy</button>
+    
+                    </div>
+                  </div>
+                </div>
+                
+      </label>
+
+
+    </section>
+<?php include("footer.php"); ?>
--- user_page/frontend/shop4.php
+++ user_page/frontend/shop4.php
@@ -1,36 +1,108 @@
-<?php include("header.php"); ?>
+<?php 
+
+include("header.php");
+include 'connect.php' ;
+
+if(isset ($_POST['add_to_cart'])){
+   $product_name=$_POST['product_name'];
+   $product_price=$_POST['product_price'];
+   $product_image=$_POST['product_image'];
+   $product_quantity=1;
+
+   // select cart data
+   $select_cart-mysqli_query($conn,"select * from 'cart' where name='$product_name'");
+   if(mysqli_num_rows($select_cart)>0){
+      $display_message[]= "product alrealdy added to cart";
+   }
+   else {
+
+   
+   insert_products=mysqli_query($conn,"insert into 'cart' (name, price, image, quantity) values
+   ('$product_name','$product_price','$product_image','$product_quantity')");
+
+   $display_message[]= "product added to cart";
+}
 
 
-<div class="container"> 
-      
-      <div class="title">PRODUCT LIST</div>
-      <div class="icon-cart">
-          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-            
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
-          </svg>
-          <span>0</span>
-      </div>
-  
-  <div class="listProduct"></div>
+
+
+?>
+
+<div class="coffee_section layout_padding">
+
+         <div class="container">
+
+            <div class="row">
+               <h1 class="coffee_taital">Coffees</h1>
+               <div class="bulit_icon"><img src="images/bulit-icon.png"></div>
+               <div class="icon-cart"><a href="cart.php" ><div>
+                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z">
+                     </a></svg>
+                  </div>
 </div>
 
-<div class="cartTab">
-  <h1>Shopping Cart</h1>
-  <div class="listCart">
+            </div>
+         </div>
+         <div class="coffee_section_2">
       
-  </div>
-  <div class="btn">
-      <button class="close">CLOSE</button>
-      <button class="checkOut">Check Out</button>
-  </div>
-</div>
+            <div id="main_slider" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+              
+                  <div class="carousel-item active">
+              
+                     <div class="container-fluid">
+                        <div class="row">
+                              <?php
+                              $select_product=mysqli_query($conn,"select id, name,price, image from 'products'");
+                              if(mysqli_num_rows($select_product)>0){
+                                 while ($fetch_product=mysqli_fetch_assoc($select_product)){
 
+                                 }
+                              }
+                     ?>
+              <form method="post" action="">
 
+                           <div class="col-lg-3 col-md-6">
+                           <div class="coffee_img"><img src="<?php echo $fetch_product['image']; ?>" ></div>
+                            <h3 class="types_text"><?php echo $fetch_product['name']; ?></h3>
+                            <p class="looking_text"><?php echo $fetch_product['price']; ?></p>
+                            <input type="hidden" name ="product_name" value="<?php echo $fetch_product['name']; ?>">
+                            <input type="hidden" name ="product_price" value="<?php echo $fetch_product['price']; ?>">
+                            <input type="hidden"  name ="product_image" value="<?php echo $fetch_product['image']; ?>">
+                            <input type="submit" class="read_bt" value="Add to Cart" name="add_to_cart">
+                  
+                    </div>
+                            </div>
+<?php
+                    }
+                          
+                         ?>
 
-<script src="js/cart.js"></script>
-</body>
+                           </div>
 
+                    
+                  </div>
+                  <div>
 
+                  
+               
+                              
+                          
+                           
+                        </div>
+                     </div>
+                     
+                </div>
 
-</html>
+               </div>
+               <a href="" class="cart">< <i class="fa-solid fa-cart-shopping"></i><span><sup>4</sup></span></a>
+               <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+               <i class="fa fa-arrow-left"></i>
+               </a>
+               <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+               <i class="fa fa-arrow-right"></i>
+               </a>
+            </div>
+         </div>
+
+         <?php include("footer.php"); ?>
